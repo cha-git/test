cc7c04fd0add5a5b0c4570376219425f1eac419f
cha-git <worksshap@gmail.com>
2018-11-29 17:04:15 +0900

    2018/11/29

---
+++ sample/src/jp/co/internous/action/Main.java
@@ -0,0 +1,12 @@
+package jp.co.internous.action;
+
+public class Main {
+
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println(gokei(2,3));
+	}
+	public static int gokei(int num1,int num2){
+		return num1+num2;
+	}
+}
